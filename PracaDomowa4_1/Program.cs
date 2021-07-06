fde33f246a90df78f70cff3f20e6ea685638af23
michal.graczykk <michal.graczykk@gmail.com>
2021-07-06 18:10:59 +0200

    Praca domowa 4.1

---
+++ PracaDomowa4_1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracaDomowa4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Podaj proszę liczbę: ");
+                int number = GetNumber();
+
+                int evenNumber = number % 2;
+
+
+                if (evenNumber == 0)
+                {
+                    Console.WriteLine("Liczba jest parzysta");
+                }
+
+                else if (evenNumber == 1)
+                {
+                    Console.WriteLine("Liczba jest nieparzysta");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+
+
+        }
+        private static int GetNumber()
+        {
+            if (!int.TryParse(Console.ReadLine(), out int number))
+            {
+                throw new Exception("Podana wartość jest nieprawidłowa!");
+            }
+            return number;
+        }
+    }
+}
